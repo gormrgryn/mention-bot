2d22fe4264ece458faa05dc909481cb4cde6f66b
gormrgryn <gor-margaryan-05@mail.ru>
2021-05-08 15:46:17 +0400

    adding functionallity for different groups

--- index.js
+++ index.js
@@ -2,20 +2,51 @@ const { readFileSync, writeFile } = require('fs')
 const token = readFileSync('./token.txt')
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf(token)
-const members = [...JSON.parse(readFileSync('./members.json')).members]
+const groups = [...JSON.parse(readFileSync('./members.json')).groups]
 
 bot.start(ctx => {
-    ctx.reply('barev xareb')
-})
-bot.command('/all', ctx => {
-    ctx.reply(members.map(i => `@${i}`).join(' '))
-
-})
-bot.on('text', ctx => {
-    members.push(ctx.message.from.username)
-    writeFile('./members.json', JSON.stringify({members}), err => {
-        if (err) throw err
+    ctx.reply('barev dzez, yes Gagon em')
+    let a = 0
+    groups.forEach(i => {
+        if(i.id != ctx.chat.id) {
+            a++
+        }
     })
+    if (a === groups.length || (groups.length === 0 && a === 0)) {
+        groups.push({
+            id: ctx.chat.id.toString(),
+            members: []
+        })
+        writeFile('./members.json', JSON.stringify({groups}), err => {
+            if (err) throw err
+        })
+    } else {
+        console.log('qez chisht pahi')
+    }
 })
+
+
+
+
+
+
+// bot.command('/all', ctx => {
+//     ctx.reply(members.map(i => `@${i}`).join(' '))
+// })
+// bot.on('text', ctx => {
+//     console.log(ctx.message.chat)
+//     let i = ctx.message.from.username
+//     if (members.includes(i)) {
+//         return
+//     }
+//     members.push(i)
+//     console.log(members)
+    
+// })
+// bot.command('/test', ctx => {
+//     console.log(ctx.chat)
+// })
+
+
 
 bot.launch()
