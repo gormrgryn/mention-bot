b2a28927b83c6b617ae30e0cdd2295c8f721a62a
gormrgryn <gor-margaryan-05@mail.ru>
2021-05-25 19:14:00 +0400

    getting ready to deploy

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { Telegraf } = require('telegraf')
 require('dotenv').config()
 
-const token = process.env.BOT_API_TOKEN || ''
+const API_TOKEN = process.env.BOT_API_TOKEN || ''
 const PORT = process.env.PORT || 3000
 const URL = process.env.URL || 'https://mention-bot-telegram.herokuapp.com'
 
-const bot = new Telegraf(token)
+const bot = new Telegraf(API_TOKEN)
 bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
 const { post, wrap, Chat, User } = require('./classes')
